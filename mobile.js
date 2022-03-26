11c864297557a145959daea0f9fa5483577f60bf
Nathan Cochran <nacochranpb@gmail.com>
2022-03-26 07:52:43 -0500

    Finished Mobile Navigation
    Added a Hamburger Menu for mobile devices.

--- mobile.js
+++ mobile.js
@@ -6,9 +6,19 @@
  */
 function mobileNavigation() {
     var links = document.getElementsByClassName("mobileLinks")[0];
-    if (links.style.display === "block") {
-      links.style.display = "none";
-    } else {
-      links.style.display = "block";
-    }
+    setTimeout(function() {
+        if (links.classList.contains("addMobile")) {
+            links.classList.add("removeMobile");
+            links.classList.remove("addMobile");
+          } else {
+            links.classList.remove("removeMobile");
+            links.classList.add("addMobile");
+          }
+    }, 25);
+  }
+
+  function closeNavigation() {
+    var links = document.getElementsByClassName("mobileLinks")[0];
+    links.classList.add("removeMobile");
+    links.classList.remove("addMobile");
   }
